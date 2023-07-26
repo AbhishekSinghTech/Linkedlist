91ff6b5a1ea6a6d81c30d3fe0cd1be9bfed80c5a
Abhishek Singh <136239184+AbhishekSinghTech@users.noreply.github.com>
2023-07-26 21:34:06 +0530

    Add files via upload

---
+++ Test5.java
@@ -0,0 +1,44 @@
+// finding position of an element in a linked list
+
+class Node{
+        int data;
+        Node next;
+        Node(int x){
+            data=x;
+            next=null;
+        }
+    }
+  
+class Test { 
+    
+    public static void main(String args[]) 
+    { 
+        Node head=new Node(10);
+    	head.next=new Node(20);
+    	head.next.next=new Node(30);
+    	printlist(head);
+    	System.out.println("Position of element in Linked List: "+search(head,20));
+    	
+    } 
+    
+    static int search(Node head, int x){
+        int pos=1;
+        Node curr=head;
+        while(curr!=null){
+            if(curr.data==x)
+                return pos;
+            else{
+                pos++;
+                curr=curr.next;
+            }
+        }
+        return -1;
+    }
+    public static void printlist(Node head){
+        Node curr=head;
+        while(curr!=null){
+        System.out.print(curr.data+" ");
+        curr=curr.next;
+    }System.out.println();
+    }
+}
